a277159eb0cb8f0b1151cf12b92cc15ecdbecf07
Daniel Flanagan <daniel@lytedev.io>
2016-06-24 02:17:41 -0500

    alright - done for now

--- planck/lytedev/keymap.c
+++ planck/lytedev/keymap.c
@@ -12,11 +12,9 @@ extern keymap_config_t keymap_config;
 #define _QWERTY  0
 #define _COLEMAK 1
 #define _LOWER   2
-#define _LOWER2  3
-#define _RAISE   4
-#define _RAISE2  5
-#define _PLOVER  6
-#define _ADJUST  7
+#define _RAISE   3
+#define _PLOVER  4
+#define _ADJUST  5
 
 // macro shortcuts
 #define QWERTY  M(_QWERTY)
@@ -24,9 +22,8 @@ extern keymap_config_t keymap_config;
 #define LOWER   M(_LOWER)
 #define RAISE   M(_RAISE)
 #define PLOVER  M(_PLOVER)
-#define ADJUST  MO(_ADJUST)
-#define LOWER2  M(_LOWER2)
-#define RAISE2  M(_RAISE2)
+#define ADJUST  OSL(_ADJUST)
+
 #define EXT_PLV M(13)
 #define TOG_OUT M(14)
 
@@ -39,49 +36,37 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		{KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
 		{KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
 		{KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT },
-		{KC_LCTL, ADJUST,  KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
+		{KC_LCTL, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
 	},
 	[_COLEMAK] = {
 		{KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC},
 		{KC_ESC,  KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT},
 		{KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT },
-		{KC_LCTL, ADJUST,  KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
+		{KC_LCTL, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
 	},
 	[_LOWER] = {
 		{KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC},
 		{KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE},
 		{_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,S(KC_NUHS),S(KC_NUBS),_______, _______, RESET},
-		{_______, _______, _______, _______, _______,  _______, _______, LOWER2, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY}
-	},
-	[_LOWER2] = {
-		{_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
-		{_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
-		{_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
-		{_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
+		{_______, _______, _______, _______, KC_TRNS, _______, _______, ADJUST,  KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY}
 	},
 	[_RAISE] = {
-		{KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL },
-		{KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS},
-		{_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, _______, _______, _______},
-		{ADJUST,  _______, _______, _______, RAISE2,  _______, _______, _______, KC_MNXT, KC_PGDN, KC_PGUP, KC_MPLY}
-	},
-	[_RAISE2] = {
-		{_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
-		{_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
-		{_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
-		{_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
+		{KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,   KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL },
+		{KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS},
+		{_______, KC_F7,   KC_F8,   KC_F9,   KC_F10, KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, _______, _______, _______},
+		{_______, _______, _______, _______, ADJUST, _______, _______, KC_TRNS, KC_MNXT, KC_PGDN, KC_PGUP, KC_MPLY}
 	},
 	[_PLOVER] = {
 		{KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1   },
 		{XXXXXXX, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC},
 		{TOG_OUT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
-		{EXT_PLV, XXXXXXX, XXXXXXX, KC_C,    KC_V,    XXXXXXX, XXXXXXX, KC_N,    KC_M,    XXXXXXX, XXXXXXX, XXXXXXX}
+		{EXT_PLV, XXXXXXX, XXXXXXX, KC_C,    KC_V,    XXXXXXX, XXXXXXX, KC_N,    KC_M,    XXXXXXX, XXXXXXX, QWERTY }
 	},
 	[_ADJUST] = {
-		{KC_J,    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  KC_DEL},
+		{_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL },
 		{MUV_IN,  _______, _______, _______, _______, _______, _______, QWERTY,  COLEMAK, PLOVER,  _______, _______},
 		{MUV_DE,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
-		{MU_TOG,  _______, AU_TOG,  _______, _______, _______, _______, _______, _______, _______, _______, RESET  }
+		{MU_TOG,  KC_TRNS, AU_TOG,  _______, _______, _______, _______, _______, _______, _______, _______, RESET  }
 	}
 };
 
